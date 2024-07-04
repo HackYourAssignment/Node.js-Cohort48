cdf0c6f0ee0fb54c9758db2d35372735b7923715
Hanna <hanna.melnk@gmail.com>
2024-07-04 20:40:37 +0200

    finished assignment for Node w2

--- assignments/hackyourtemperature/server.js
+++ assignments/hackyourtemperature/server.js
@@ -1,21 +1,7 @@
-import express from 'express';
-
-//create an instance of express application
-const app = express();
-
-//middleware to parse json
-app.use(express.json());
-
-//set up route to handle GET requests
-app.get('/', (req, res) => {
-   res.send(`hello from backend to frontend!`)
-});
-app.post('/weather', (req, res) => {
-   const { cityName } = req.body; //destructure request body to get the city name
-   res.send(`City name received: ${cityName}`);
-});
-
+import app from './app.js'
 const PORT = 3000;
+
+
 //make server listen to port 3000
 app.listen(PORT, () => {
    console.log(`Server is running on http://localhost:${PORT}`);
---
+++ assignments/hackyourtemperature/sources/keys.js
@@ -0,0 +1,3 @@
+const keys = {API_KEY: '76e58a1eb5d8e71aff3b4456f4d13920'};
+
+export default keys;
